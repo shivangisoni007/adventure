d7641662d70ab6940e65773b06f58995b517cb9b
shivangisoni007 <shivangisoni007@gmail.com>
2020-09-26 11:55:03 +0530

    Updates

---
+++ precache-manifest.d20804700bda3425791a3ac0475265de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7427bf91f8b63876dacc8d906c8c6b",
+    "url": "/adventure/index.html"
+  },
+  {
+    "revision": "2660d600bab408ad23e9",
+    "url": "/adventure/static/css/main.33c30dbb.chunk.css"
+  },
+  {
+    "revision": "81f406e8c9af5db3b228",
+    "url": "/adventure/static/js/2.4fd06500.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adventure/static/js/2.4fd06500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2660d600bab408ad23e9",
+    "url": "/adventure/static/js/main.0651818d.chunk.js"
+  },
+  {
+    "revision": "c391c9a197f334e4a83e",
+    "url": "/adventure/static/js/runtime-main.c1ae067e.js"
+  }
+]);
