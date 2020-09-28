6a24d15da7b328967b6ff6d94ce6b19a1c9231fe
shivangisoni007 <shivangisoni007@gmail.com>
2020-09-28 19:10:54 +0530

    commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,9 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video >
-                <source src="https://shivangisoni007.github.io/public/videos/video-1.mp4" autoplay></source>
-            </video>
+            <video src='/videos/video-1.mp4' autoPlay loop muted />
             <h1>ADVENTURE AWAITS</h1>
             <p> What are you waitng for?</p>
             <div className='hero-btns'>
