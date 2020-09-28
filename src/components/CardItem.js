697858f57965ae9c2a11513d34bd82a89ac640b0
shivangisoni007 <61016703+shivangisoni007@users.noreply.github.com>
2020-09-28 18:39:02 +0530

    Update CardItem.js

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -9,6 +9,7 @@ function CardItem(props) {
                <figure className='cards__item__pic-wrap' data-category={props.label}>
                    <img
                    className='cards__item__img' 
+        alt={"heel"}
                    src={props.src}
                    />
                </figure>
